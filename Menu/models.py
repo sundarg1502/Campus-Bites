34088ce45ad12f6b081a0fde4aed7b7476b9e557
sundarg1502 <sundarg1502@gmail.com>
2025-01-21 22:03:38 +0530

    fix : fix the js function to send data async and poplate them in db

--- Menu/models.py
+++ Menu/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import os
 import datetime 
 
@@ -53,8 +54,8 @@ class Product(models.Model):
         return self.name
     
 class Cart(models.Model):
-    name = models.ForeignKey(Product,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     quantity = models.IntegerField()
-    price = models.IntegerField()
-    total = models.IntegerField()
     status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
--- Menu/views.py
+++ Menu/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render,HttpResponse
-from Menu.models import Product
+from django.shortcuts import render
+from django.http import JsonResponse
+from Menu.models import Product,Cart
+import json
 
 # Create your views here.
 def index(request):
@@ -10,5 +12,10 @@ def cart(request):
     return render(request,'cart.html')
 
 def addtocart(request):
-    print(request.GET.get("id"))
-    return HttpResponse("Suucescc")
+    if request.user.is_authenticated:
+        data = json.load(request)
+        pid = Product.objects.get(pk=data['id'])
+        Cart.objects.create(product=pid,user=request.user,quantity=data['qty'])
+        return JsonResponse({"Ststus":'Successs'},status=200)
+    else:
+        return JsonResponse({"status":"User Login Required"},status=200)
